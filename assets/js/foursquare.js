b84823662e5b6242c687c3110b124944b628c943
cblawler <chris.b.lawler@gmail.com>
2019-06-25 11:12:58 -0500

    magical unicorns

--- assets/js/foursquare.js
+++ assets/js/foursquare.js
@@ -2,16 +2,16 @@
 
 var userlat = 0;
 var userlng = 0;
-var resultlatlngs=[]; //lat, lng
+var resultlatlngs = []; //lat, lng
 
 
 navigator.geolocation.getCurrentPosition(StorePosition);
 
 function StorePosition(position) {
 
-  userlat = position.coords.latitude;
-  userlng = position.coords.longitude;
-  foursquareapicall();
+    userlat = position.coords.latitude;
+    userlng = position.coords.longitude;
+    foursquareapicall();
 
 
 }
@@ -20,7 +20,7 @@ function StorePosition(position) {
 //Working api call
 
 https://api.foursquare.com/v2/venues/search?client_id=PNXMQSVLHPQQUTUYQLGUPVZOMX1LSE5NPHRWVF1NG3PINRQU&client_secret=FRSFYMBNKSKMI34AIKNI1CEBRMHDLYRHQJB5WHSV1PZCKTJA&v=20180323&limit=10&ll=32.8387891,-96.7857465&categoryId=4bf58dd8d48988d1e5941735&radius=32000
-function foursquareapicall () {
+function foursquareapicall() {
 
     var queryURL = "https://api.foursquare.com/v2/venues/search";
 
@@ -31,46 +31,47 @@ function foursquareapicall () {
             client_secret: "FRSFYMBNKSKMI34AIKNI1CEBRMHDLYRHQJB5WHSV1PZCKTJA",
             v: "20180323",
             limit: "5",
-            ll: userlat+","+userlng,
-            categoryId:"4bf58dd8d48988d1e5941735",
+            ll: userlat + "," + userlng,
+            categoryId: "4bf58dd8d48988d1e5941735",
             radius: "3000",
         },
         method: "GET",
 
     })
 
-    .then(function (response) {
-        var venueresults = response.response.venues;
-        resultlatlngs=[];
-        for (var i = 0; i < venueresults.length; i++) {
-            console.log("what up");
-        // console.log(i)
-        // console.log(response)
-        // console.log(response.response)
-        // console.log(response.response.venues)
-        // console.log(response.response.venues[i])
-        console.log(venueresults[i]) ;
-        console.log("name is " + venueresults[i].name) ;
-        resultlatlngs.push([venueresults[i].location.lat, venueresults[i].location.lng]);
-        console.log(venueresults[i].location.address);
-        console.log(venueresults[i].location.city) ;
-        console.log(venueresults[i].location.state) ;
-        console.log(venueresults[i].location.postalCode) ;
-
-       
-
-        var name = venueresults[i].name;
-        var address = venueresults[i].location.address;
-        var city = venueresults[i].location.city;
-        var state = venueresults[i].location.state;
-        var postalCode=venueresults[i].location.postalCode;
-
-
-        var resultsdiv = $("<div>");
+        .then(function (response) {
+            var venueresults = response.response.venues;
+            resultlatlngs = [];
+            for (var i = 0; i < venueresults.length; i++) {
+                console.log("what up");
+                console.log([venueresults[i].location.lat, venueresults[i].location.lng]);
+                // console.log(i)
+                // console.log(response)
+                // console.log(response.response)
+                // console.log(response.response.venues)
+                // console.log(response.response.venues[i])
+                console.log(venueresults[i]);
+                console.log("name is " + venueresults[i].name);
+                resultlatlngs.push([venueresults[i].name, location.lat, venueresults[i].location.lng]);
+                console.log(venueresults[i].location.address);
+                console.log(venueresults[i].location.city);
+                console.log(venueresults[i].location.state);
+                console.log(venueresults[i].location.postalCode);
 
 
 
-        resultsdiv.html(`
+                var name = venueresults[i].name;
+                var address = venueresults[i].location.address;
+                var city = venueresults[i].location.city;
+                var state = venueresults[i].location.state;
+                var postalCode = venueresults[i].location.postalCode;
+
+
+                var resultsdiv = $("<div>");
+
+
+
+                resultsdiv.html(`
         <div class="card bg-dark text-white">
         <img class="card-img" src="assets/images/dog-park.jpg" alt="Card image">
             <div class="card-img-overlay">
@@ -81,23 +82,23 @@ function foursquareapicall () {
             </div> </div>
         `);
 
-        $("#results").append(resultsdiv);
+                $("#results").append(resultsdiv);
 
+                runMap(venueresults);
 
+                //start with frozen image and store animated location. start with still state
+                // var gifimage = $("<img>");
+                // gifimage.attr("src", results[i].images.fixed_height_still.url);
+                // gifimage.attr("data-still", results[i].images.fixed_height_still.url);
+                // gifimage.attr("data-animate", results[i].images.fixed_height.url);
+                // gifimage.attr("data-state", "still");
+                // gifimage.addClass("gif");
+                // resultsdiv.prepend(p);
+                //         resultsdiv.prepend(gifimage);
 
-        //start with frozen image and store animated location. start with still state
-        // var gifimage = $("<img>");
-        // gifimage.attr("src", results[i].images.fixed_height_still.url);
-        // gifimage.attr("data-still", results[i].images.fixed_height_still.url);
-        // gifimage.attr("data-animate", results[i].images.fixed_height.url);
-        // gifimage.attr("data-state", "still");
-        // gifimage.addClass("gif");
-        // resultsdiv.prepend(p);
-        //         resultsdiv.prepend(gifimage);
-
-        //         $("#gifzone").prepend(gifDiv);
+                //         $("#gifzone").prepend(gifDiv);
             }
-            console.log(resultlatlngs);
+            // console.log(resultlatlngs);
         })
 }
 
--- assets/js/googlemaps.js
+++ assets/js/googlemaps.js
@@ -1,1 +1,65 @@
 
+function runMap(results) {
+    console.log(results);
+    // return;
+    var locations = [];
+
+    results.forEach(function (ele) {
+        console.log(ele);
+        locations.push([
+            ele.name,
+            ele.location.lat,
+            ele.location.lng
+        ]);
+
+
+    });
+    var googleURL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCVuViz2cE5eJRQwQPm9YVHTtsIhkLpsMc";
+    console.log(locations);
+
+    $.ajax({
+        url: googleURL,
+        method: 'GET',
+        dataType: 'jsonp'
+    }).then(function () {
+        // 32.7767° N, 96.7970° W
+        var latitude = 32.7767;
+        var longitude = -96.7970;
+        var x = parseFloat(latitude);
+        var y = parseFloat(longitude);
+        console.log("got here")
+        // var locations = [
+        //     // ['Preston Hollow Park', 32.8804037, -96.7952787],
+        //     // ['Four Seasons', 32.864327, -96.95764]
+        // ];
+
+        //this is now a static loading map with no pins
+
+        var latlng = new google.maps.LatLng(x, y);
+        var myOptions = {
+            zoom: 13,
+            center: latlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map(document.getElementById('map'), myOptions);
+
+        var infowindow = new google.maps.InfoWindow();
+
+        var marker, i;
+
+        for (i = 0; i < locations.length; i++) {
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+                map: map
+            });
+
+            google.maps.event.addListener(marker, 'click', (function (marker, i) {
+                return function () {
+                    infowindow.setContent(locations[i][0]);
+                    infowindow.open(map, marker);
+                }
+            })(marker, i));
+        };
+    });
+    console.log(resultlatlngs);
+}
